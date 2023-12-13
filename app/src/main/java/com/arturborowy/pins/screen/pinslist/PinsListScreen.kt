4de0e90c65913f7efee5978662683d3f9ffcc618
Artur Borowy <artur.borowy843@gmail.com>
2023-12-13 17:35:05 +0100

    Basic pin list

--- app/src/main/java/com/arturborowy/pins/screen/pinslist/PinsListScreen.kt
+++ app/src/main/java/com/arturborowy/pins/screen/pinslist/PinsListScreen.kt
@@ -1,33 +1,41 @@
 package com.arturborowy.pins.screen.pinslist
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
-import com.arturborowy.pins.R
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun PinsListScreen() {
-    Column(
+fun PinsListScreen(viewModel: PinsListViewModel = hiltViewModel()) {
+    viewModel.onViewResume()
+
+    val state = viewModel.state.collectAsState()
+
+    val addressDetails = state.value.placeDetails
+
+    LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.purple_700))
+            .fillMaxWidth()
             .wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Pins list",
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        items(addressDetails) {
+            Button(
+                onClick = { viewModel.onAddressClick(it) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = it.label)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/arturborowy/pins/screen/pinslist/PinsListViewModel.kt
@@ -0,0 +1,36 @@
+package com.arturborowy.pins.screen.pinslist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arturborowy.pins.model.places.PlaceDetails
+import com.arturborowy.pins.model.places.PlacesInteractor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PinsListViewModel @Inject constructor(
+    private val placesInteractor: PlacesInteractor
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(State())
+    val state: StateFlow<State> = _state
+
+
+    fun onViewResume() {
+        viewModelScope.launch {
+            val places = placesInteractor.getPlaces()
+            _state.emit(State(places))
+        }
+    }
+
+    fun onAddressClick(placeDetails: PlaceDetails) {
+
+    }
+
+    data class State(
+        val placeDetails: List<PlaceDetails> = listOf(),
+    )
+}
