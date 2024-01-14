148291bc59b82b2a28c3060fdb9cdc5c3507761c
Artur Borowy <artur.borowy843@gmail.com>
2024-01-14 23:43:13 +0100

    [#12] Implement new splash screen for android >=12.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
     implementation("com.google.dagger:hilt-android:2.48.1")
     implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
