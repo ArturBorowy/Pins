63c885f55884bc96e4b20e5f250603c2c77cfb5e
Artur Borowy <artur.borowy843@gmail.com>
2026-03-08 19:22:12 +0100

    Use rememberSaveable in AllTripsMap to not pick new random marker on device configuration change

--- app/src/main/java/com/arturborowy/pins/ui/composable/map/Map.kt
+++ app/src/main/java/com/arturborowy/pins/ui/composable/map/Map.kt
@@ -1,14 +1,15 @@
 package com.arturborowy.pins.ui.composable.map
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedEffect
 import com.arturborowy.brand.designsystem.BrandTheme
 import com.arturborowy.pins.screen.map.MapMarkerItem
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Polyline
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun Map(
@@ -17,9 +18,20 @@ fun Map(
     cameraLatitude: Double,
     cameraLongitude: Double,
 ) {
-    val cameraLatLng = remember { LatLng(cameraLatitude, cameraLongitude) }
-    val cameraPosition =
-        remember { CameraPositionState(CameraPosition.fromLatLngZoom(cameraLatLng, zoom)) }
+    val cameraPosition = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(cameraLatitude, cameraLongitude), zoom)
+    }
+
+    LaunchedEffect(cameraLatitude, cameraLongitude) {
+        cameraPosition.move(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    cameraLatitude,
+                    cameraLongitude
+                ), zoom
+            )
+        )
+    }
 
     GoogleMap(
         cameraPositionState = cameraPosition,
