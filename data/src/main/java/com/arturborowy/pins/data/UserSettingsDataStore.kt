be3c5abbdcf73390f47671e5bc2de60486335ef3
Artur Borowy <artur.borowy843@gmail.com>
2026-03-11 19:24:13 +0100

    Add useDynamicColors to UserSettingsDataStore and create UserSettingsRepository.

--- data/src/main/java/com/arturborowy/pins/data/UserSettingsDataStore.kt
+++ data/src/main/java/com/arturborowy/pins/data/UserSettingsDataStore.kt
@@ -2,6 +2,7 @@ package com.arturborowy.pins.data
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -9,17 +10,30 @@ import javax.inject.Inject
 class UserSettingsDataStore @Inject constructor(private val dataStore: DataStore<Preferences>) {
 
     companion object {
-        private val APP_THEME = stringPreferencesKey("APP_THEME")
+        private val APP_VISUAL_THEME = stringPreferencesKey("APP_VISUAL_THEME")
+        private val USE_DYNAMIC_COLORS = booleanPreferencesKey("USE_DYNAMIC_COLORS")
     }
 
-    fun appThemeFlow() = dataStore.data.map { preferences ->
-        preferences[APP_THEME]
+    fun appVisualThemeFlow() = dataStore.data.map { preferences ->
+        preferences[APP_VISUAL_THEME]
     }
 
-    suspend fun setAppTheme(newValue: String) {
+    suspend fun setAppVisualTheme(newValue: String) {
         dataStore.updateData {
             it.toMutablePreferences().also { preferences ->
-                preferences[APP_THEME] = newValue
+                preferences[APP_VISUAL_THEME] = newValue
+            }
+        }
+    }
+
+    fun useDynamicColorsFlow() = dataStore.data.map { preferences ->
+        preferences[USE_DYNAMIC_COLORS]
+    }
+
+    suspend fun setUseDynamicColors(newValue: Boolean) {
+        dataStore.updateData {
+            it.toMutablePreferences().also { preferences ->
+                preferences[USE_DYNAMIC_COLORS] = newValue
             }
         }
     }
