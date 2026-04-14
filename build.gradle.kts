d641e77cbbafe157235930d91624ead5ce1e448f
Artur Borowy <artur.borowy843@gmail.com>
2026-04-14 15:40:56 +0200

    Introduce detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "9.0.0" apply false
     id("com.android.library") version "9.0.0" apply false
@@ -6,4 +5,12 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
     id("com.google.devtools.ksp") version "2.3.0" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    source.setFrom(files(projectDir))
+    config.setFrom("$projectDir/detekt/detekt.yml")
 }
