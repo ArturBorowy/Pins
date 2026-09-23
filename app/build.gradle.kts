160f761bc92cfcaa3ac8e9d063b52276455f4e7e
Artur Borowy <artur.borowy843@gmail.com>
2026-09-23 19:20:57 +0200

    Use Google Secrets Plugin for storage of Google Maps api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
     id("com.google.devtools.ksp") version "2.3.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
 
 detekt {
